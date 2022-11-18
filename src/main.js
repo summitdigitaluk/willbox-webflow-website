e24dd90c0fb6ccaec35e73e4d526c505ab93543d
Carl Burden <carl@summit-digital.co.uk>
2022-11-18 16:30:45 +0000

    Moved OwlCarousel so that it loads on scroll

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 //Make sure the content behind the Mega Nav or a modal does not scroll
 import 'body-scroll-lock';
-import 'owl.carousel';
 
 import { arrayIsEmpty, setEnquiryButton } from '$utils/helper-functions';
 
@@ -97,33 +96,4 @@ $('#addEnquiry').on('click', function (e) {
     '[{"billing":"hire","id":"refrigeratedcontainernastartdate2022119enddate20221231","name":"Refrigerated Container","img":"https://assets-global.website-files.com/624f4fe3fe9e978f68b9adfe/62dab2ee8a990f3699aea2fe_Willbox%20Containers%20-%2010%27%20Reefer-WBXU_EXTERNAL_MACHINE_UNIT_1024px.jpg","variant":"n/a","qty":"3","selectedDates":["2022-11-09T00:00:00.000Z","2022-12-31T00:00:00.000Z"]},{"billing":"hire","id":"platformcontainernastartdate2022119enddate20221231","name":"Platform Container","img":"https://assets-global.website-files.com/624f4fe3fe9e978f68b9adfe/62daafb47a56d7ecd863ff31_Willbox%20Containers%20-%2020%27%20Platform%20-%20WBXU_VIEW_002_1024px.jpg","variant":"n/a","qty":"1","selectedDates":["2022-11-09T00:00:00.000Z","2022-12-31T00:00:00.000Z"]},{"billing":"purchase","id":"securestoragecontainer20ft","name":"Secure Storage Container","img":"https://assets-global.website-files.com/624f4fe3fe9e978f68b9adfe/62dab9f28a990f9bd7aef157_Willbox%20Containers%20-%2020%27-WBXU_EXTERNAL_1024px.jpg","variant":"20ft","qty":"3"},{"billing":"hire","id":"sleepercabinnastartdate2022119enddate20221231","name":"Sleeper Cabin","img":"https://assets-global.website-files.com/624f4fe3fe9e978f68b9adfe/62dab068a16b1a6a3b958275_Willbox%20Containers%20-%2020%27%20Double%20Sleeper%20Cabin%20-%20WBXU_INTERNAL_1024px.jpg","variant":"n/a","qty":"2","selectedDates":["2022-11-09T00:00:00.000Z","2022-12-31T00:00:00.000Z"]},{"billing":"hire","id":"dryingroomnastartdate2022119enddate20221231","name":"Drying Room","img":"https://assets-global.website-files.com/624f4fe3fe9e978f68b9adfe/62daae81152ea2ae4e0c17f5_Willbox%20Containers%20-%2010%27%20Drying%20Room%20-WBXU_INTERNAL_1024px.jpg","variant":"n/a","qty":"2","selectedDates":["2022-11-09T00:00:00.000Z","2022-12-31T00:00:00.000Z"]},{"billing":"purchase","id":"securestoragecontainer45ft","name":"Secure Storage Container","img":"https://assets-global.website-files.com/624f4fe3fe9e978f68b9adfe/62dab9f28a990f9bd7aef157_Willbox%20Containers%20-%2020%27-WBXU_EXTERNAL_1024px.jpg","variant":"45ft","qty":"4"},{"billing":"purchase","id":"securestoragecontainer10ft","name":"Secure Storage Container","img":"https://assets-global.website-files.com/624f4fe3fe9e978f68b9adfe/62dab9f28a990f9bd7aef157_Willbox%20Containers%20-%2020%27-WBXU_EXTERNAL_1024px.jpg","variant":"10ft","qty":"2"}]'
   );
   window.location.reload();
-});
-
-loadJsFiles([
-  {
-    source: 'https://apps.elfsight.com/p/platform.js',
-    condition: () => {
-      if ($('#elfsight-google-reviews').length > 0) {
-        return true;
-      }
-    },
-    ignoreOnMobile: true,
-    success: null,
-    fail: () => {
-      $('#elfsight-google-reviews').remove();
-    },
-  },
-]);
-
-$('.collection-list-partners.w-dyn-items').owlCarousel({
-  margin: 20,
-  nav: false,
-  items: 4,
-  loop: true,
-  autoWidth: true,
-  center: true,
-  autoplay: true,
-  autoplayHoverPause: true,
-  autoplayTimeout: 3000,
-  autoplaySpeed: 2000,
 });
